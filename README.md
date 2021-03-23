cd3bfa5033ca5b8898cd7ddf901b597a126d5e79
Diego Lisboa <diego.lisboa.pires@hotmail.com>
2021-03-22 21:10:35 -0300

    Projeto Simple Mooc Django 3 e Python 3

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# SimpleMooc - Gerenciador de Cursos Web
+
+## Projeto Web estilo SimpleMooc usando Django 3 e Python 3
---
+++ simplemooc/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import home, contato
+
+app_name = 'core' # Necessário para o reconhecimento do namespace
+urlpatterns = [
+    path('', home, name="home"),
+    path('contato/', contato, name="contato"),
+]
