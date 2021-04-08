9caf0a7f09b9fd2fc043918f80c4691118516c7d
Diego Lisboa <diego.lisboa.pires@hotmail.com>
2021-04-08 15:48:25 -0300

    Page de contato e view e model do curso criados

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'clear_cache',
+    'core',
+    'cursos'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,14 @@ WSGI_APPLICATION = 'simplemooc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'simplemooc',
+        'USER': 'postgres',
+        'PASSWORD': 'thegame05',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/' 
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
