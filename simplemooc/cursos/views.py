73c73b6145b9d940e62060c0d0c13758ef111369
Diego Lisboa <diego.lisboa.pires@hotmail.com>
2021-04-10 22:04:50 -0300

    Alteraçoes para envio de email

--- simplemooc/cursos/views.py
+++ simplemooc/cursos/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Cursos
+from .forms import ContatoCursoForm
 
 # Create your views here.
 
 def cursos(request):
-    return render(request, 'cursos/cursos.html', {'cursos': Cursos.objects.all()})
+    return render(request, 'cursos/cursos.html', {
+        'cursos': Cursos.objects.all(),
+    })
 
 def detalhes_curso(request, slug): #pk
     # get_object_or_404() -> Retornar o objeto caso exista se não retornar a page 404
-    return render(request, 'cursos/detalhes.html', {'curso': get_object_or_404(Cursos, slug=slug)}) #pk=pk
+    if request.method == 'POST':
+        formContato = ContatoCursoForm(request.POST)
+
+        if formContato.is_valid():
+            print(formContato.cleaned_data)
+    else:
+        formContato = ContatoCursoForm()
+    return render(request, 'cursos/detalhes.html', {
+            'curso': get_object_or_404(Cursos, slug=slug),
+            'formContato': formContato
+    }) #pk=pk
--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -129,3 +129,13 @@ USE_TZ = True
 STATIC_URL = '/static/' 
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
+
+# Envio de Email Django
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
+EMAIL_PORT = 587
